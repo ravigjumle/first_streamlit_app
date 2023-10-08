8b6598d82717eb64f92d7663279b2ef8c5086fa9
ravigjumle <147332692+ravigjumle@users.noreply.github.com>
2023-10-08 21:36:53 +0530

    Update streamlit_app.py
    update emoji and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit
 
+streamline.title(‘My Mom’s new Healthy Diner’)
+
+streamline.header(‘Breakfast Favorites’)
+streamlit.text(‘🥣 Omega 3 & Blueberry Oatmeal’)
+streamlit.text(‘🥗 Kale, Spinach & Rocket Smoothie’)
+streamlit.text(‘🐔 Hard-Boiled Free-Range Egg’)
+streamlit.text(‘🥑🍞 Avocado Toast’)
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
