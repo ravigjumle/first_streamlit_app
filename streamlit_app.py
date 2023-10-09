6b1b097544fb3e30110b6645ae6ef7766e97ba3c
ravigjumle <147332692+ravigjumle@users.noreply.github.com>
2023-10-09 13:45:11 +0530

    Update streamlit_app.py
    Pick the fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Mom New Healthy Diner')
 
@@ -11,5 +11,14 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+The picker works, but the numbers don't make any sense! We want the customer to be able to choose the fruits by name!!
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
